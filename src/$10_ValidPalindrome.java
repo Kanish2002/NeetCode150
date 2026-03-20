dfb19690a54bbd356a06daa2799b7151e04b5e03
Kanish <nkrish2208@gmail.com>
2026-03-21 01:02:13 +0530

    Problem 10 :  Valid Palindrome O(n) time and O(1) Solution added

--- src/$10_ValidPalindrome.java
+++ src/$10_ValidPalindrome.java
@@ -16,3 +16,122 @@ class $10_ValidPalindrome1 {
         return true;
     }
 }
+class $10_ValidPalindrome2 {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while(left < right){
+            char l = s.charAt(left);
+            char r = s.charAt(right);
+
+            boolean isLeftValid = false;
+            boolean isRightValid = false;
+
+            if(isAlphaNumeric(l)){
+                l = toLower(l);
+                isLeftValid = true;
+            }
+            if(isAlphaNumeric(r)){
+                r = toLower(r);
+                isRightValid = true;
+            }
+            if(!isLeftValid && isRightValid) {
+                left++;
+                continue;
+            }
+            if(isLeftValid && !isRightValid){
+                right--;
+                continue;
+            }
+
+            if(isLeftValid && isRightValid){
+                if(l != r) return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public char toLower(char c) {
+        if(c >= 'A' && c <= 'Z') c+=32;
+        return c;
+    }
+    public boolean isAlphaNumeric(char c){
+        if((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')) return true;
+        return false;
+    }
+}
+
+class $10_ValidPalindrome3 {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while(left < right){
+            char l = s.charAt(left);
+            char r = s.charAt(right);
+
+            boolean isLeftValid = false;
+            boolean isRightValid = false;
+
+            if(isAlphaNumeric(l)) isLeftValid = true;
+            if(isAlphaNumeric(r)) isRightValid = true;
+
+            if(!isLeftValid) {
+                left++;
+                continue;
+            }
+            if(!isRightValid){
+                right--;
+                continue;
+            }
+
+            if(isLeftValid && isRightValid){
+                if(toLower(l) != toLower(r)) return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public char toLower(char c) {
+        if(c >= 'A' && c <= 'Z') c+=32;
+        return c;
+    }
+    public boolean isAlphaNumeric(char c){
+        if((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')) return true;
+        return false;
+    }
+}
+
+class $10_ValidPalindrome4 {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while(left < right){
+            char l = s.charAt(left);
+            char r = s.charAt(right);
+            if(!isAlphaNumeric(l)) {
+                left++;
+                continue;
+            }
+            if(!isAlphaNumeric(r)){
+                right--;
+                continue;
+            }
+            if(toLower(l) != toLower(r)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public char toLower(char c) {
+        if(c >= 'A' && c <= 'Z') c+=32;
+        return c;
+    }
+    public boolean isAlphaNumeric(char c){
+        if((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')) return true;
+        return false;
+    }
+}
+
+
